96faacbb763a3991820854ed93c65912fe97cacf
Redbeard29 <ben@benshermanmusic.com>
2021-03-21 16:06:32 -0400

    basic style changes and started to display some of the small JS elements on the page

--- hangman.js
+++ hangman.js
@@ -1,19 +1,22 @@
-const word_array = ["Aegon/the/Conqueror", "Arya/Stark", "The/Battle/of/the/Bastards", "Beric/Dondarrion", "Brandon/Stark", 
-"Brienne/of/Tarth", "Bronn", "Casterly/Rock", "Catelyn/Stark", "Cersei/Lannister", "Chaos/is/a/ladder", "The/Children", 
-"Daario/Naharis", "Daenerys/Targaryen", "Davos/Seaworth", "Dorne", "Dragonglass", "The/Drowned/God", "Eddard/Stark", "Edmure/Tully", 
-"Ellaria/Sand", "The/Eyrie", "The/Free/Folk", "The/Fist/of/the/First/Men", "Gendry/Baratheon", "Grey/Worm", "Joffrey/Baratheon", 
-"Gilly/Craster", "A/Girl/is/no/one", "Highgarden", "The/High/Septon", "Hodor", "I/drink/and/I/know/things", 
-"If/you/think/this/has/a/happy/ending/you/havent/been/paying/attention", "Iron/Islands", "The/Iron/Throne", 
-"Jaime/Lannister", "Jaqen/Hghar", "Jojen/Reed", "Jon/Snow", "Jorah/Mormont", "Khal/Drogo", "King/Robert", "Kings/Landing", 
-"Lady/Melisandre", "A/Lannister/always/pays/his/debts", "Loras/Tyrell","Longclaw", "The/Lord/of/Light", "Lysa/Arryn", 
-"The/man/who/passes/the/sentence/should/swing/the/sword", "Margaery/Tyrell", "Mhysa", "Missandei", "Mother/of/Dragons", 
-"Myrcella/Baratheon", "Never/forget/what/you/are/bastard", "Night/gathers/and/now/my/watch/begins", "The/night/is/dark/and/full/of/terrors", 
-"The/Night/King", "The/North/Remembers", "Nymeria", "Oberyn/Martell", "Osha", "Olenna/Tyrell", "Podrick/Payne", "Petyr/Baelish", "Pyke", 
-"Qyburn", "Ramsay/Bolton", "Riverrun", "Roose/Bolton", "Robb/Stark", "Storms/End", "Samwell/Tarly", "Sansa/Stark", "The/Sept/of/Baelor", 
-"Shae", "Stannis/Baratheon", "Syrio/Forel", "Tommen/Baratheon", "Tormund/Giantsbane", "Tyrion/Lannister", "Theon/Greyjoy", "The/Hound", 
-"The/Mountain", "The/Sand/Snakes", "Tywin/Lannister", "Viserys/Targaryen", "The/Lord/of/Light", "Lord/Varys", "Walder/Frey", 
-"What/do/we/say/to/the/god/of/death?Not/Today", "Where/are/my/Dragons?", "White/Walkers", "Winterfell", "Winter/is/coming", "Xaro/Xhoan/Daxos", 
-"You/know/nothing/Jon/Snow", "Ygritte"];
+const word_array = ["Aegon the Conqueror", "Arya Stark", "The Battle of the Bastards", "Beric Dondarrion", "Brandon Stark", 
+"Brienne of Tarth", "Bronn", "Casterly Rock", "Catelyn Stark", "Cersei Lannister", "Chaos is a ladder", "The Children", 
+"Daario Naharis", "Daenerys Targaryen", "Davos Seaworth", "Dorne", "Dragonglass", "The Drowned God", "Eddard Stark", "Edmure Tully", 
+"Ellaria Sand", "The Eyrie", "The Free Folk", "The Fist of the First Men", "Gendry Baratheon", "Grey Worm", "Joffrey Baratheon", 
+"Gilly Craster", "A Girl is no one", "Highgarden", "The High Septon", "Hodor", "I drink and I know things", 
+"If you think this has a happy ending you haven't been paying attention", "Iron Islands", "The Iron Throne", 
+"Jaime Lannister", "Jaqen Hghar", "Jojen Reed", "Jon Snow", "Jorah Mormont", "Khal Drogo", "King Robert", "King's Landing", 
+"Lady Melisandre", "A Lannister always pays his debts", "Loras Tyrell","Longclaw", "The Lord of Light", "Lysa Arryn", 
+"The man who passes the sentence should swing the sword", "Margaery Tyrell", "Mhysa", "Missandei", "Mother of Dragons", 
+"Myrcella Baratheon", "Never forget what you are bastard", "Night gathers and now my watch begins", "The night is dark and full of terrors", 
+"The Night King", "The North Remembers", "Nymeria", "Oberyn Martell", "Osha", "Olenna Tyrell", "Podrick Payne", "Petyr Baelish", "Pyke", 
+"Qyburn", "Ramsay Bolton", "Riverrun", "Roose Bolton", "Robb Stark", "Storm's End", "Samwell Tarly", "Sansa Stark", "The Sept of Baelor", 
+"Shae", "Stannis Baratheon", "Syrio Forel", "Tommen Baratheon", "Tormund Giantsbane", "Tyrion Lannister", "Theon Greyjoy", "The Hound", 
+"The Mountain", "The Sand Snakes", "Tywin Lannister", "Viserys Targaryen", "The Lord of Light", "Lord Varys", "Walder Frey", 
+"What do we say to the god of death? Not Today", "Where are my Dragons?", "White Walkers", "Winterfell", "Winter is coming", "Xaro Xhoan Daxos", 
+"You know nothing Jon Snow", "Ygritte"];
+
+let guesses = 6;
+let wins = 0;
 
 function getWord(array){
     const randIdx = Math.floor(Math.random() * array.length);
@@ -22,9 +25,8 @@ function getWord(array){
 }
 
 
-function displayBoard(tries){
-    stages = [
-        
-    ];
-}
 console.log(getWord(word_array));
+
+document.getElementById('guesses').innerHTML += guesses;
+document.getElementById('wins').innerHTML += wins;
+document.getElementById('word_spotlight').innerHTML = getWord(word_array);
